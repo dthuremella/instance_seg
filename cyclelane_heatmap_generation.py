579c33f6da7f01a25b5837d35b0b3a1eb47a02c5
dthuremella <divyat@alumni.princeton.edu>
2025-02-26 13:36:09 +0000

    start north-loop at 1000 not 100

--- cyclelane_heatmap_generation.py
+++ cyclelane_heatmap_generation.py
@@ -203,7 +203,8 @@ def main():
     bad_vals = [v  for  v in  bad_dict.values()]
     bad_vals.sort()
     filename_to_timestamp = {}
-    for i in range(100, len(bad_keys)):
+    start_frame = 100 if map_name != 'northloop' else 1000
+    for i in range(start_frame, len(bad_keys)):
         key = bad_keys[i][:16]
         val = bad_vals[i]
         filename_to_timestamp[key] = val
